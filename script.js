0d9c5018dcd69f8081eaab9d54b6eaca9bafa8b3
PC2015-13\SDW <enab@hotmail.fr>
2018-03-02 11:26:16 +0100

    blabla

---
+++ script.js
@@ -0,0 +1,2 @@
+    // ce petit script ajoute une image en haut du
+    $('body').prepend('<img src="./supdeweb-logo.jpg" alt="Sup de Web" style="width: 350px; margin: 50px 0 50px 0;" class="img-rounded">');
